27adb0fe6afc15b12ed67f2e1898fb8c61a2136c
Pattarawat Chormai <goonnow@rocketmail.com>
2013-11-03 14:26:01 +0700

    Fix complaining about missing radix.

--- app/scripts/controllers/deduction.js
+++ app/scripts/controllers/deduction.js
@@ -20,7 +20,7 @@ angular.module('taxApp')
         var sum = 0;
         angular.forEach( $scope.list, function(data, key){
             if( data.value ) {
-                sum += parseInt(data.value);
+                sum += parseInt(data.value, 10);
             }
         });
 

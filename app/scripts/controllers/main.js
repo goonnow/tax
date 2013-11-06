362be82282419190c4cad3f6e2f70d79cf83b295
Pattarawat Chormai <goonnow@rocketmail.com>
2013-11-06 22:00:05 +0700

    Now, we can calcurate tax!!.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,7 +6,17 @@ app.controller('MainCtrl', function ($scope) {
     $scope.income =0;
     $scope.deduction =0;
 
-    $scope.diff = function(){
-        return $scope.income - $scope.deduction;
-    };
+    $scope.$watch( 'income', function(){
+        updateData();
+    });
+
+    $scope.$watch( 'deduction', function(){
+        updateData();
+    });
+
+    function updateData() {
+        $scope.diff = $scope.income - $scope.deduction;
+        $scope.tax = TAX($scope.diff);
+    }
+
 });
